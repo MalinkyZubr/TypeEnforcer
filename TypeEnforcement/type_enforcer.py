51017774f7a7ed9864b287567b00baf60cddc79c
MalinkyZubr <ahumanbeing189@gmail.com>
2023-03-15 23:24:58 -0700

    added lots of fixes for recursive type enforcement to support UnionType within recursive function

--- TypeEnforcement/type_enforcer.py
+++ TypeEnforcement/type_enforcer.py
@@ -24,25 +24,32 @@ class TypeEnforcer:
             except TypeError:
                 pass
             print(type(expected_type))
-            if recursive and type(expected_type) == types.UnionType:
+            if received_type in typing.get_args(expected_type) or expected_type == typing.Any:
+                continue
+            elif recursive and type(expected_type) == types.UnionType:
                 type_origins_union = [expected for expected in typing.get_args(expected_type) if type(expected) == types.GenericAlias and received_type == expected.__origin__]
                 if not type_origins_union and not return_check:
                     raise exc.WrongParameterType(func.__name__,argument_name,received_type,expected_type)
                 elif not type_origins_union:
                     raise exc.WrongReturnType(expected_type, received_type)
                 elif len(type_origins_union) == 1:
-                    TypeEnforcer.__generic_alias_checker(type_origins_union[0], argument, argument_name, func.__name__, is_return=return_check)
+                    TypeEnforcer.__generic_alias_checker(type_origins_union[0], type_origins_union[0], argument, argument_name, func.__name__, is_return=return_check)
                 else: # MIGRATE TO THE RECURSIVE TYPE CHECKER FUNCTION
-                    fail_count
+                    fail_count = 0
                     for expected in type_origins_union:
-
+                        try:
+                            TypeEnforcer.__generic_alias_checker(expected, expected, argument, argument_name, func.__name__, is_return=return_check)
+                            break
+                        except (exc.WrongParameterType, exc.WrongReturnType,):
+                            fail_count += 1
+                        if fail_count == len(type_origins_union) and return_check:
+                            raise exc.WrongReturnType(expected_return_type=expected_type, actual_return_type=received_type)
+                        elif fail_count == len(type_origins_union) and not return_check:
+                            raise exc.WrongParameterType(function_name=func.__name__, parameter_name=argument_name,received_type=received_type, expected_type=expected_type)
 
             elif recursive and type(expected_type) == types.GenericAlias:
-                TypeEnforcer.__generic_alias_checker(expected_type, argument, argument_name, func.__name__, is_return=return_check)
-            elif (received_type != expected_type 
-                and received_type not in typing.get_args(expected_type)
-                and expected_type != typing.Any
-                ):
+                TypeEnforcer.__generic_alias_checker(expected_type, expected_type, argument, argument_name, func.__name__, is_return=return_check)
+            elif (received_type != expected_type):
                 if not return_check:
                     raise exc.WrongParameterType(func.__name__,argument_name,received_type,expected_type)
                 raise exc.WrongReturnType(expected_type, received_type)
@@ -77,8 +84,7 @@ class TypeEnforcer:
         return complete_hints, return_type
     
     @staticmethod
-    def __generic_alias_checker(data_type: types.GenericAlias | types.UnionType, data: typing.Any, parent_variable_name: str, func_name: str, is_return: bool=False):
-        # PUT THE UNION TYPE ANALYSIS HERE
+    def __generic_alias_checker(data_type: types.GenericAlias, parent_data_type: types.GenericAlias, data: typing.Any, parent_variable_name: str, func_name: str, is_return: bool=False, previous_index=""):
         if type(data_type) == types.GenericAlias:
             if type(data) != data_type.__origin__:
                 if is_return:
@@ -87,22 +93,24 @@ class TypeEnforcer:
                     raise exc.WrongParameterType(func_name, parent_variable_name, type(data), data_type)
             if isinstance(data, typing.Iterable) and not isinstance(data, str) and not isinstance(data, dict):
                 if len(data_type.__args__) == 1:
-                    for item in data:
-                        TypeEnforcer.__generic_alias_checker(data_type.__args__[0], item, parent_variable_name, func_name, is_return)
+                    for index, item in enumerate(data):
+                        TypeEnforcer.__generic_alias_checker(data_type.__args__[0], parent_data_type, item, parent_variable_name, func_name, is_return, previous_index=previous_index+f"[{index}]")
                 elif len(data) == len(data_type.__args__):
-                    for dtype, item in zip(data_type.__args__, data):
-                        TypeEnforcer.__generic_alias_checker(dtype, item, parent_variable_name, func_name, is_return)
+                    for index, dtype_item_tup in enumerate(zip(data_type.__args__, data)):
+                        dtype, item = dtype_item_tup
+                        TypeEnforcer.__generic_alias_checker(dtype, parent_data_type, item, parent_variable_name, func_name, is_return, previous_index=previous_index+f"[{index}]")
                 else:
                     raise AttributeError(f"The argument {parent_variable_name} received a {data_type} of length {len(data)} when it wanted a length of {len(data_type.__args__)}")
             elif isinstance(data, dict):
-                for key, value in data.items():
-                    TypeEnforcer.__generic_alias_checker(data_type.__args__[0], key, parent_variable_name, func_name, is_return)
-                    TypeEnforcer.__generic_alias_checker(data_type.__args__[1], value, parent_variable_name, func_name, is_return)
+                for index, value in data.items():
+                    TypeEnforcer.__generic_alias_checker(data_type.__args__[0], parent_data_type, index, parent_variable_name, func_name, is_return, previous_index=previous_index+f"[{index}]")
+                    TypeEnforcer.__generic_alias_checker(data_type.__args__[1], parent_data_type, value, parent_variable_name, func_name, is_return, previous_index=previous_index+f"[{index}]")
         elif data_type != type(data) and data_type != typing.Any:
+            print("FUCKED HERE")
             if is_return:
-                raise exc.WrongReturnType(data_type, type(data))
+                raise exc.WrongReturnType(f'{data_type} nested inside {parent_variable_name}', type(data), position=f"{previous_index} in {parent_data_type}")
             else:
-                raise exc.WrongParameterType(func_name, f"Nested variable in {parent_variable_name}", type(data), data_type)
+                raise exc.WrongParameterType(func_name, f"Nested variable in {parent_variable_name}", type(data), data_type, position=f"{previous_index} in {parent_data_type}")
 
     @staticmethod
     def enforcer(recursive:bool=False):
@@ -144,13 +152,6 @@ class TypeEnforcer:
 
                 return_value = func(*args, **kwargs)
                 TypeEnforcer.__check_args({'return':return_type}, {'return':return_value}, func, recursive=recursive, return_check=True)
-                # if return_type != typing.Any:
-                #     print("RETURN")
-                #     if type(return_type) == types.GenericAlias:
-                #         print("running check")
-                #         TypeEnforcer.__generic_alias_checker(return_type, return_value, 'return', func.__name__, is_return=True)
-                #     elif type(return_value) != return_type and return_type not in typing.get_args(return_type) and return_type != typing.Any:
-                #         raise exc.WrongReturnType(return_type, type(return_value))
                 return return_value
             return inner
         return enforcement
@@ -164,7 +165,7 @@ if __name__ == "__main__":
         pass
 
     @TypeEnforcer.enforcer(recursive=True)
-    def foo(n, t: tuple[int, str], h: dict[str,int], v: typing.Callable, f: list[str], x: typing.Any, y: str, z: typing.Optional[bool]=True, a: str="hello") -> list[str]:
+    def foo(n, t: tuple[int, str] | dict[int,str], h: dict[str,int] | int, v: typing.Callable, f: list[str, int], x: typing.Any, y: str, z: typing.Optional[bool]=True, a: str="hello") -> list[str]:
         return ["hi"]
 
     class zeug:
@@ -173,13 +174,13 @@ if __name__ == "__main__":
             pass
 
         @TypeEnforcer.enforcer(recursive=True)
-        def ziggle(self, x: dict) -> tuple[str, int, str, tuple[int, str]] | dict:
-            return ("he", 1, "ho", (1, "ze"))
+        def ziggle(self, x: dict) -> tuple[str, int, str, tuple[int, str]] | dict[str,str]:
+            return {"":""}#("he", 1, "ho", (1, "2"))
 
     def zoo():
         pass
 
-    x = foo(Doof(), (2,"hi"), {"x":1} , zoo, ['r', 'r'], 1, "hi", z=None)
+    x = foo(Doof(), (2,"hi"), 1 , zoo, ['r', 1], 1, "hi", z=None)
     print(x)
 
     z = zeug((1, ""))
